75aacb03e5e62d0eaec26c77c32d6a93edea2302
Juan Jose Duran <78617304+juanjduran@users.noreply.github.com>
2024-03-19 13:59:41 -0300

    Add files via upload

---
+++ uno_a_cien.c
@@ -0,0 +1,13 @@
+/*
+Hacer un programa que cuente desde el 1 al 100 y los imprima uno a uno.
+*/
+
+#include<stdio.h>
+
+void main(){
+    int number = 1;
+
+    for(number; number<101; number++){
+        printf("%d\n", number);
+    }
+}
